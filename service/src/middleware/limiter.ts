9f24d250b84fcbc5eea9e26765be46df43c5e8a3
unknown <896559147@qq.com>
2023-03-25 14:37:23 +0800

    fix: 小时内限制数增加中文提示

--- service/src/middleware/limiter.ts
+++ service/src/middleware/limiter.ts
@@ -12,7 +12,7 @@ const limiter = rateLimit({
   max: maxCount,
   statusCode: 200, // 200 means success，but the message is 'Too many request from this IP in 1 hour'
   message: async (req, res) => {
-    res.send({ status: 'Fail', message: 'Too many request from this IP in 1 hour', data: null })
+    res.send({ status: 'Fail', message: '1个小时内请求次数太多，请稍后再试（Too many request from this IP in 1 hour）', data: null })
   },
 })
 
